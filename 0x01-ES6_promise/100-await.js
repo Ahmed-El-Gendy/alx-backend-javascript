908bbb9578ad4f1fb547de696473fab1500b33f8
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-07 21:25:23 +0000

    adv

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default async function asyncUploadUser() {
+  let ans = {};
+  try {
+    const p = await uploadPhoto();
+    const u = await createUser();
+    ans = { p, u };
+  } catch (err) {
+    ans = { p: null, u: null };
+  }
+  return ans;
+}
