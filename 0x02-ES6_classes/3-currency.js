d98883ce5a264e61f2811537671528c9f84298fd
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-10 17:29:03 +0000

    task3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,6 +1,32 @@
-export default class Currency {
-    constructor(code, name) {
-        this._code = code;
-        this._name = name;
+export default class HolbertonCourse {
+  constructor(code, name) {
+    this.name = name;
+    this.code = code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'number') {
+      throw new TypeError('Code must be a number');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
