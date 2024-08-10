f9466a367f9aa7c464f2988c1ce275c8b55725a3
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-10 17:15:08 +0000

    task2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,13 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+    this._students = students;
+  }
+}
