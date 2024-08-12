4cf768638d54707fbf642eb3ad045881abaf223f
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-12 22:50:21 +0000

    task7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,4 @@
 export default function hasValuesFromArray(s, arr) {
-  const newS = new Set(s);
-  const newArrS = new Set(arr);
-  const inter = newS.intersection(newArrS);
-  return newArrS.size === inter.size;
+  const ch = arr.every((el) => s.has(el));
+  return ch;
 }
