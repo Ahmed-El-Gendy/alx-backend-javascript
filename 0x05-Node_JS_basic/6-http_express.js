80d345569a02836c8e746c5122681c00c2c99c87
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-10-04 13:21:32 +0000

    task6

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+app.length('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+module.exports = app;
