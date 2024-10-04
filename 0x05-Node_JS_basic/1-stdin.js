d9196314c14113287302edf7e2620969bc1a9ec1
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-10-04 12:43:37 +0000

    task0

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,10 @@
-/*
-executed through command line
-*/
-
 console.log("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data}`);
+  process.stdout.write(`Your name is: ${data}\n`);
+  process.stdin.end();
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing\n');
 });
