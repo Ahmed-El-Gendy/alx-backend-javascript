5e40383aa13ba7c758727f5a8567145f8b4d7ef2
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-10-04 12:54:16 +0000

    task3"

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    const studentRecords = lines.slice(1);
+
+    if (studentRecords.length === 0) {
+      console.log('Number of students: 0');
+      return;
+    }
+
+    const fieldData = {};
+
+    studentRecords.forEach((record) => {
+      const fields = record.split(',');
+
+      if (fields.length === 4) {
+        const firstname = fields[0];
+        const field = fields[3];
+
+        if (!fieldData[field]) {
+          fieldData[field] = [];
+        }
+
+        fieldData[field].push(firstname);
+      }
+    });
+
+    const totalStudents = studentRecords.length;
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const [field, students] of Object.entries(fieldData)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
